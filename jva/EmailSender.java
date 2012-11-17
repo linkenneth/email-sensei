afb489665c1ff05e784c7cef82e7d73d831df7b0
michaelv03 <michael.lin.yang@berkeley.edu>
2012-11-17 03:25:25 -0800

    failed to connect no password specified?

--- jva/EmailSender.java
+++ jva/EmailSender.java
@@ -22,19 +22,20 @@ public class EmailSender {
     public void connect(String username, String passwd, String email,
                                String host, int port) {
 
-        USERNAME = username;  // rube.goldberg
-        PASSWORD = passwd;  // n0n0n0n0n0
-        EMAIL = email;  // rube.goldberg@gmail.com
-        HOST = host;  // imap.gmail.com
+        USERNAME = username;  // goldberg.chain.reaction
+        PASSWORD = passwd;  // n0n0n0n0
+        EMAIL = email;  // goldberg.chain.reaction@gmail.com
+        HOST = host;  // smtp.gmail.com
         PORT = port;  // 465
 
         Properties props = new Properties();
-        props.put("mail.imaps.host", HOST);
-        props.put("mail.imaps.debug", "true");
-        props.put("mail.imaps.starttls.enable", "true");
-        props.put("mail.imaps.auth", "true");
+        props.put("mail.smtp.host", HOST);
+        props.put("mail.smtp.debug", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.ssl.enable", "true");
         props.put("mail.store.protocol", "imaps");
-        this.session = Session.getInstance(props, new SMTPAuthenticator());
+        session = Session.getInstance(props, new SMTPAuthenticator());
 
     }
 
@@ -44,7 +45,7 @@ public class EmailSender {
         MimeMessage msg = new MimeMessage(session);
 
         InternetAddress sender = new InternetAddress(EMAIL);
-        msg.setFrom(sender);
+        msg.setSender(sender);
 
         InternetAddress[] recipients = new InternetAddress[] {
             new InternetAddress(to)
--- jva/Main.java
+++ jva/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main (String args[]) {
         EmailSender emailer = new EmailSender();
         emailer.connect("goldberg.chain.reaction", "n0n0n0n0",
-                        "goldberg.chain.reaction@gmail.com", "imap.gmail.com",
+                        "goldberg.chain.reaction@gmail.com", "smtp.gmail.com",
                         465);
         try{
             emailer.send("michaelv03@gmail.com", "Test", "Hello testing");
